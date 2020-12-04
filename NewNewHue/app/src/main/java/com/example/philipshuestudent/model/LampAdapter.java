2a4f864e9e49d77897b04e2315d3d8877009cdea
fabjuuuh <f.degroot1@student.avans.nl>
2020-12-04 15:19:03 +0100

    deleted

--- NewNewHue/app/src/main/java/com/example/philipshuestudent/model/LampAdapter.java
+++ NewNewHue/app/src/main/java/com/example/philipshuestudent/model/LampAdapter.java
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.philipshuestudent.R;
-import com.example.philipshuestudent.ui.SecondFragment;
 
 import java.util.ArrayList;
 
